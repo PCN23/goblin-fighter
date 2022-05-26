25821312998263582fab5753799686bce44e3fb3
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-26 16:04:59 -0700

    adding new form elemnts

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 // import functions and grab DOM elements
+const form = document.querySelector('form');
 
 // let state
+let playerHP = 10;
+let goblins = [
+    { name: 'Harry', hp: 1 },
+    { name: 'Mike', hp: 4 },
+];
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const goblinName = data.get('goblin-name');
+
+    const newGoblin = {
+        name: goblinName,
+        hp: Math.cell(Math.random() * 5),
+    };
+
+    goblinName.push(newGoblin);
+    displayGoblins();
+
+});
+
+function displayGoblins() {
+    goblinListEl.textContent = '';
+
+    for (let goblin of goblins) {
+        const goblinEl = renderGoblin(goblin);
+
+        goblinEl.addEventListener('click', () => {
+            goblinClickHandler(goblin);
+        });
+        goblinListEl.append(goblinEl);
+    }
+}
+
+displayGoblins();
+
